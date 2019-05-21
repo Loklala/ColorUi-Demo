2f3b8e4bd1163d9845ea6da1bed58ffd6ac8b703
jinlin <1120550982@qq.com>
2019-05-21 21:02:00 +0800

    as

--- common/common.vue
+++ common/common.vue
@@ -1,6 +1,6 @@
 <script type="text/javascript">
-    // 定义一些公共的属性和方法
-    const httpUrl = 'http://192.168.0.199:8080'
+    // 定义一些公共的属性和方法http://192.168.0.199:8080
+    const httpUrl = 'http://testadmin.xkegame.com'
     function commonFun() {
         console.log("公共方法")
     }
